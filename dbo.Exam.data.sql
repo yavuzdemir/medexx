c0cc222a642a12995735bca023a9ea38a92ca592
yavuz ozdemir <yavuz ozdemir@YAVUZ>
2017-01-13 19:52:21 +0000

    Add project files.

---
+++ dbo.Exam.data.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO [dbo].[Exam] ([Exam_ID], [ExamType_ID], [Patient_ID], [Exam_Name], [Price], [Fasting], [Smoke], [Alcoholic_Drinks]) VALUES (1, 1, 1, N'Urine', 4, NULL, N'24h', N'24h')
+INSERT INTO [dbo].[Exam] ([Exam_ID], [ExamType_ID], [Patient_ID], [Exam_Name], [Price], [Fasting], [Smoke], [Alcoholic_Drinks]) VALUES (2, 6, 2, N'Electrocardiogram', 20, NULL, NULL, NULL)
+INSERT INTO [dbo].[Exam] ([Exam_ID], [ExamType_ID], [Patient_ID], [Exam_Name], [Price], [Fasting], [Smoke], [Alcoholic_Drinks]) VALUES (3, 3, 3, N'Endoscopy', 25, NULL, NULL, NULL)
+INSERT INTO [dbo].[Exam] ([Exam_ID], [ExamType_ID], [Patient_ID], [Exam_Name], [Price], [Fasting], [Smoke], [Alcoholic_Drinks]) VALUES (4, 2, 4, N'Magnetic Ressonance', 35, NULL, NULL, NULL)
